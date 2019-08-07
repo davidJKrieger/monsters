6f09ab4ebd7582b2f7fadb720972e950d7885475
David Krieger <davidkrieger@Davids-MacBook-Air.local>
2019-08-06 19:35:37 -0600

    added cards

--- src/components/card-list/card-list-component.jsx
+++ src/components/card-list/card-list-component.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './card-list.styles.css';
+import { Card } from '../card.component'
 
-const CardList = (props) => {
-    console.log(props)
-    return <div className='card-list'>{props.children}</div>
-};
-
+const CardList = (props) => (
+    <div className='card-list'>
+        {props.monsters.map(monster=>(
+            <Card key={monster.id} monster={monster}/>
+        ))}
+    </div>
+);
+   
 export default CardList;
---
+++ src/components/card.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './card-list.styles.css';
+
+const Card = () => {
+    <div>
+        <h1 key={monster.id}>{monster.name}</h1>
+    </div>
+}
+
+export default Card
